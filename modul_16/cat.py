eeb91037f84efe190129bf71e50659e4d55a5eec
Matroskin2004 <102403502+Matroskin2004@users.noreply.github.com>
2022-09-07 23:12:24 +0300

    Create cat.py

---
+++ modul_16/cat.py
@@ -0,0 +1,8 @@
+class cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def cat_print(self):
+        print(f"Имя: {self.name}\nПол: {self.gender}\nВозраст: {self.age}")
