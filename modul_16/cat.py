2d52f128d1980251e9c975152c166bb39d7a053a
Matroskin2004 <102403502+Matroskin2004@users.noreply.github.com>
2022-09-07 23:24:09 +0300

    Delete modul_16 directory

--- modul_16/cat.py
+++
@@ -1,8 +0,0 @@
-class cat:
-    def __init__(self, name, gender, age):
-        self.name = name
-        self.gender = gender
-        self.age = age
-
-    def cat_print(self):
-        print(f"Имя: {self.name}\nПол: {self.gender}\nВозраст: {self.age}")
