5b1555188bb9a308dbc7100b18f6cdf62cc99e57
Matroskin2004 <102403502+Matroskin2004@users.noreply.github.com>
2022-10-25 21:33:56 +0300

    Add files via upload

---
+++ Modul_18/config.py
@@ -0,0 +1,9 @@
+TOKEN = '5528037116:AAGV-oQouu_yHrJ46OQlhNoxr4UvNaRI0iA'
+
+api_key= 'R4uejliPnCCttOrU6wPnX0B8kB6S6ggI '
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
